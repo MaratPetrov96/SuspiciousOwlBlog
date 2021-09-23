693ff1e5ca8f47854149fad0816892c6767ff7ce
MaratPetrov96 <64157774+MaratPetrov96@users.noreply.github.com>
2021-09-23 09:25:44 +0700

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path,include
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
@@ -7,5 +8,12 @@ urlpatterns = [
     #path('test',views.test,name='link'),
     path('p=<int:id_>',views.page),
     path('<int:pk>',views.RecordView.as_view(),name='one'),
-    path('about',views.about)
+    path('<int:pk>/edit',views.RecordUpdate.as_view(),name='edit'),
+    path('about',views.about),
+    path('sign',views.signup),
+    path('test',views.test),
+    path('login',views.LogIn,name='login'),
+    path('logout',views.LogOut,name='logout'),
+    path('comment/<int:pk>',views.comment,name='comment'),
+    path('like/<int:pk>', views.like, name='like')
 ]
