f549673e251dbbbe81ae02d42a9a33845b21ec14
MaratPetrov96 <64157774+MaratPetrov96@users.noreply.github.com>
2021-09-23 09:26:48 +0700

    Comment form added

--- main/forms.py
+++ main/forms.py
@@ -1,12 +1,24 @@
-from .models import Record
-from django.forms import ModelForm,TextInput,Textarea,DateTimeInput,ImageField
-
 class AddForm(ModelForm):
     class Meta:
         model=Record
         fields=['title','text','anons','picture']
-        widgets={'title':TextInput(attrs={'name':'link'}),
-                 'anons':Textarea(attrs={'name':'anons','rows':'2','cols':'20'}),
-                 'text':Textarea(attrs={'name':'title',
-                                        'rows':'8','cols':'40'}),
-                 'picture':ImageField()}
+        widgets={'title':TextInput(attrs={'name':'link','placeholder':''}),
+                 'anons':Textarea(attrs={'name':'anons','rows':'2','cols':'20',
+                                         'placeholder':''}),
+                 'text':Textarea(attrs={'name':'title','rows':'10','cols':'60'
+                                        ,'placeholder':''})}
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, help_text='Optional.')
+    last_name = forms.CharField(max_length=30, help_text='Optional.')
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+
+class CommentForm(ModelForm):
+    class Meta:
+        model=Comment
+        fields=['content']
+        widgets={'content':Textarea(attrs={'name':'title','rows':'10','cols':'60'
+                                        ,'placeholder':'Ваш комментарий'})}
